36cafa3d3146d7cc530b42fbfbc252efc94acd22
TianXing Xue <76930817+veritas0518@users.noreply.github.com>
2021-06-24 08:46:52 +0800

    Copy of binary file
    【问题描述】读取默认目录下的图片QQ.png，将其拷贝到当前目录下生成一个新的名为QQBak.png文件，QQBak.png文件的内容与QQ.png完全一致。
    
    【样例输入】当前目录下的图片文件 QQ.png
    
    【样例输出】当前目录下的图片文件 QQBak.png

---
+++ SHIYAN5_3.java
@@ -0,0 +1,44 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class SHIYAN5_3 {
+
+    public static void main(String[] args) {
+
+        FileInputStream fp1;
+
+        FileOutputStream fp2;
+
+        try{
+
+            fp1 = new FileInputStream("QQ.png");
+
+            fp2 = new FileOutputStream("QQBak.png");
+
+           byte a[] = new byte[16];
+
+           while(fp1.available()!=0)
+{
+               fp1.read(a);
+
+               fp2.write(a);
+
+
+           }
+
+           fp1.close();
+
+           fp2.close();
+
+        }catch(IOException e){
+
+            System.out.println("exception!");
+
+        }
+    }
+}
+
+
+
+
